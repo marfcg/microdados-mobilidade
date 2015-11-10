458276f94200fec5da0f46b3676c6ad2897c1d4a
Marcelo F C Gomes <marfcg@gmail.com>
2015-11-10 20:43:20 -0200

    [Work in progress] Updated model-analysis.py

--- model-analysis/model-analysis.py
+++ model-analysis/model-analysis.py
@@ -8,7 +8,6 @@ mobility in Brazilian municipalities.
 
 import pandas as pd
 import numpy as np
-from collections import defaultdict
 
 ufacron2name = {'RO': 'RONDÔNIA', 'AC': 'ACRE', 'AM': 'AMAZONAS', 'RR': 'RORAIMA', 'PA': 'PARÁ', 'AP': 'AMAPÁ',
                 'TO': 'TOCANTINS', 'MA': 'MARANHÃO', 'PI': 'PIAUÍ', 'CE': 'CEARÁ', 'RN': 'RIO GRANDE DO NORTE',
@@ -18,18 +17,18 @@ ufacron2name = {'RO': 'RONDÔNIA', 'AC': 'ACRE', 'AM': 'AMAZONAS', 'RR': 'RORAIM
                 'MT': 'MATO GROSSO', 'GO': 'GOIÁS', 'DF': 'DISTRITO FEDERAL'}
 ufname2acron = {v: k for k, v in ufacron2name.items()}
 
-def readtable():
-  
+def readtable(uflist):
+
     dfmobility = pd.DataFrame()
     dfpop = pd.DataFrame()
     for uf in sorted(uflist):
-        dftmp = pd.read_csv('../data/%s-matriz-mobilidade-microdados.csv')
+        dftmp = pd.read_csv('../data/%s-mobility-matrix-microdata.csv' % uf)
         dftmppop = dftmp[['Origin Municipality', 'Origin FU', 'Population']].drop_duplicates().reset_index()
         for name, acron in ufname2acron.items():
             dftmp.loc[dftmp['Destination FU'] == name, 'Destination FU'] = acron
 
         dftmp.loc[(dftmp['Destination Country'] != 'BRASIL') & (-dftmp['Destination Country'].isnull()),
-                  'Destination UF'] = dftmp.loc[(dftmp['Destination Country'] != 'BRASIL') &
+                  'Destination FU'] = dftmp.loc[(dftmp['Destination Country'] != 'BRASIL') &
                                                 (-dftmp['Destination Country'].isnull()),
                                                 'Destination Country']
 
@@ -43,8 +42,9 @@ def readtable():
 
     return dfmobility, dfpop
 
-def cleantable(dfmobility):
+def cleantable(dfmobilityin):
 
+    dfmobility = dfmobilityin.copy()
     # Create list of unknowns
     unknown_list = ["%s,NÃO SABE MUNICÍPIO" % (uf) for uf in uflist] # Knows FU, does not know Municipality
     unknown_list.append("IGNORADO")                                  # No info
@@ -55,17 +55,57 @@ def cleantable(dfmobility):
     dfknown_cols = ['Origin Municipality', 'Origin FU', 'Destination Country', 'Destination FU', 'Total', 'Std error']
     grpby_cols = ['Origin Municipality', 'Origin FU', 'Destination Country', 'Destination FU']
 
-    # Aggregate knowns
-    dfknown = dfmobility.loc[(-dfmobility['Total'].isnull()) &
-                             (-dfmobility['Destination Municipality'].isin(unknown_list)), dfknown_cols]\
-        .groupby(grpby_cols).sum().reset_index().copy()
+    # # Aggregate knowns
+    # dfknown = dfmobility.loc[(-dfmobility['Total'].isnull()) &
+    #                          (-dfmobility['Destination Municipality'].isin(unknown_list)), dfknown_cols]\
+    #     .groupby(grpby_cols).sum().reset_index().copy()
 
     # Redistribute unkowns by known frequencies, respecting FU when known.
-    # Keep "multiple destinations" as a separate entity since it's an unkown of different nature.
+    # Keep "multiple destinations" as a separate entity since it's an unknown of different nature.
     for orig in ufacron2name:
-        dfknown_fu = dfknown[dfknown['Origin FU'] == orig]
-        dfmobility_fu = dfknown[dfknown['Origin FU'] == orig]
-        for origmun in dfknown.loc[dfknown['Origin FU'] == orig, 'Origin Municipality']:
+        dfmobility_fu = dfmobility[dfmobility['Origin FU'] == orig]
+        for origmun in dfmobility_fu['Origin Municipality'].unique():
+
+            # "FU,NÃO SABE MUNICÍPIO", can be any destination in the specific FU, in Brazil:
+            for ufdest in ufacron2name:
+                charval = "%s NÃO SABE MUNICÍPIO" % (ufdest)
+                val = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Total'].\
+                    where(dfmobility_fu['Destination Municipality'] == charval).dropna().to_frame(name='Total')
+                val['Std error'] = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Std error'].\
+                    where(dfmobility_fu['Destination Municipality'] == charval).dropna().to_frame(name='Std error')
+                if val.size == 1:
+                    # Redistribute over known FU destinations from origmun
+                    dftmp = dfmobility_fu.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                                              (dfmobility_fu['Destination FU'] == ufdest) &
+                                              (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
+                                              ['Total', 'Std error']]
+                    known_dest = dftmp.Total.sum()
+                    dftmp['Total'] += dftmp['Total']*float(val['Total'])/known_dest
+                    dftmp['Std error'] += dftmp['Std error']*float(val['Std error'])/known_dest
+                    dfmobility_fu.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                                      (dfmobility_fu['Destination FU'] == ufdest) &
+                                      (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
+                                      ['Total', 'Std error']] = dftmp
+            
+            # "NaN", can be any destination in Brazil:
+            val = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Total'].\
+                where(dfmobility_fu['Destination Municipality'] == np.nan).dropna().to_frame(name='Total')
+            val['Std error'] = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Std error'].\
+                where(dfmobility_fu['Destination Municipality'] == np.nan).dropna().to_frame(name='Std error')
+            if val.size == 1:
+                # Redistribute over known Brazilian destinations from origmun
+                dftmp = dfmobility_fu.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                                          (dfmobility_fu['Destination Country'] == 'BRASIL') &
+                                          (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
+                                          ['Total', 'Std error']]
+                known_dest = dftmp.Total.sum()
+                dftmp['Total'] += dftmp['Total']*float(val['Total'])/known_dest
+                dftmp['Std error'] += dftmp['Std error']*float(val['Std error'])/known_dest
+                dfmobility.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                               (dfmobility_fu['Origin FU'] == orig) &
+                               (dfmobility_fu['Destination Country'] == 'BRASIL') &
+                               (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
+                               ['Total', 'Std error']] = dftmp
 
             # "IGNORADO", can be any destination:
             val = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Total'].\
@@ -73,33 +113,18 @@ def cleantable(dfmobility):
             val['Std error'] = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Std error'].\
                 where(dfmobility_fu['Destination Municipality'] == 'IGNORADO').dropna().to_frame(name='Std error')
             if val.size == 1:
-                dftmp = dfmobility.loc[(dfmobility['Origin Municipality'] == origmun) &
-                                       (dfmobility['Origin FU'] == orig) &
-                                       (-dfmobility['Destination Municipality'].isin(unknown_list)),
+                # Redistribute over full set of known destinations from origmun
+                dftmp = dfmobility.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                                       (dfmobility_fu['Origin FU'] == orig) &
+                                       (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
                                        ['Total', 'Std error']]
-                known_dest = dfknown_fu.Total[dfknown_fu['Origim Municipality'] == origmun].sum()
-                dftmp['Total'] += dftmp*float(val['Total'])/known_dest
-                dftmp['Std error'] += dftmp*float(val['Std error'])/known_dest
+                known_dest = dftmp.Total.sum()
+                dftmp['Total'] += dftmp['Total']*float(val['Total'])/known_dest
+                dftmp['Std error'] += dftmp['Std error']*float(val['Std error'])/known_dest
+                dfmobility.loc[(dfmobility_fu['Origin Municipality'] == origmun) &
+                               (dfmobility_fu['Origin FU'] == orig) &
+                               (-dfmobility_fu['Destination Municipality'].isin(unknown_list)),
+                               ['Total', 'Std error']] = dftmp
+        dfmobility.loc[dfmobility['Origin FU'] == orig,] = dfmobility_fu.loc[dfmobility['Origin FU'] == orig,]
 
-            # "NaN", can be any destination in Brazil:
-            val = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Total'].\
-                where(dfmobility_fu['Destination Municipality'] == np.nan).dropna().to_frame(name='Total')
-            val['Std error'] = dfmobility_fu.loc[dfmobility_fu['Origin Municipality'] == origmun, 'Std error'].\
-                where(dfmobility_fu['Destination Municipality'] == np.nan).dropna().to_frame(name='Std error')
-            if val.size == 1:
-                dftmp = dfmobility.loc[(dfmobility['Origin Municipality'] == origmun) &
-                                       (dfmobility['Origin FU'] == orig) &
-                                       (dfmobility['Destination Country'] == 'BRASIL')
-                                       (-dfmobility['Destination Municipality'].isin(unknown_list)),
-                                       ['Total', 'Std error']]
-                known_dest = dfknown_fu.Total[(dfknown_fu['Origim Municipality'] == origmun) &
-                                              (dfknown_fu['Destination Country'] == 'BRASIL')].sum()
-                dftmp['Total'] += dftmp*float(val['Total'])/known_dest
-                dftmp['Std error'] += dftmp*float(val['Std error'])/known_dest
-
-            # "FU,NÃO SABE MUNICÍPIO", can be any destination in the specific FU, in Brazil:
-
-
-        for dest, val in table[orig].items():
-            if dest not in unknown_list:
-                known_dest[orig] += val
+    return dfmobility
